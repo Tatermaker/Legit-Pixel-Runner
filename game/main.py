6edd447360d4f18f815c362d792dd5fd305a3f9e
Tatermaker <157066024+Tatermaker@users.noreply.github.com>
2024-12-10 19:41:40 -0500

    game(main): actually did the thing i said i did in the last commit

--- game/main.py
+++ game/main.py
@@ -6,4 +6,5 @@ screen = pygame.display.set_mode((800,400))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT
+            pygame.quit()
     pygame.display.update()
